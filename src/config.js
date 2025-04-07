f493f2173c129f427a5df5e5954e98a7767de0c2
luka <edulukalortk@gmail.com>
2025-04-07 17:44:44 +0400

    added storage path

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 import dotenv from "dotenv";
+import { join } from "path";
 dotenv.config();
 
 const websites = process.env.WEBSITES?.split(",") || [];
 const peerId =
   process.env.PEER_ID || "peer-" + Math.random().toString(36).slice(2);
+const storagePath = process.env.STORAGE_PATH || join("storage");
 
-export { websites, peerId };
+export { websites, peerId, storagePath };
